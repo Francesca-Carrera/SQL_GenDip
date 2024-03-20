66306b9a7b57e247bb48200ba466f0c862d10ec3
Francesca Carrera <francescacarrera@protonmail.com>
2024-03-20 18:20:30 +0100

    ultimato parte di analisi dei dati

--- analisi_politicaEsteraFemminista_Italia.sql
+++ analisi_politicaEsteraFemminista_Italia.sql
@@ -62,20 +62,100 @@ WITH ROLLUP -- La clausola WITH ROLLUP ti consentirà  di ottenere anche il total
 ORDER BY 
     Y.year ASC;
 
--- Crescita percentuale maggiore di diplomatici di genere maschile in Italia. ?????
-WITH CTE AS (
-			SELECT year, maleDiplomats, male_prevYearCount, male_diff,
-				   CAST(ROUND(((CONVERT(DECIMAL(18,2), male_diff) / male_prevYearCount) * 100), 2) AS DECIMAL(18,2)) AS pct
-			FROM (
-				SELECT year, 
-					   maleDiplomats,
-					   LAG(maleDiplomats) OVER (ORDER BY Year) AS male_prevYearCount,
-					   maleDiplomats - LAG(maleDiplomats) OVER (ORDER BY Year) AS male_diff
-				FROM VW_diplomats_GROUP_BY_gender) AS sub)
-SELECT CONCAT(MAX(pct), '%') AS malePct_MAX
-FROM CTE;
+-- Crescita percentuale maggiore di diplomatici di genere maschile in Italia.
+
 -- 2014 172 - 2013 105 = 67 - 67 /195 (2013) * 100 = 63,81% 
--- Crescita de 2014 rispetto al 2013
+-- Crescita del 2014 rispetto al 2013
+
+-- 1) risultato senza anno
+WITH DiplomatsCounts AS (
+    SELECT 
+        Y.year, 
+        COUNT(D.gender) AS maleDiplomats
+    FROM 
+        dbo.targetArea AS TA
+        LEFT JOIN dbo.sendingCountries AS SC ON TA.sendingCountryID = SC.sendingCountryID
+        LEFT JOIN dbo.vw_country_GROUP_BY AS VW ON SC.countryID = VW.countryID
+        LEFT JOIN dbo.diplomats AS D ON TA.diplomatID = D.diplomatID
+        LEFT JOIN dbo.years AS Y ON TA.yearID = Y.yearID
+    WHERE 
+        VW.alpcode = 'ITA' AND D.gender = 0
+    GROUP BY 
+        Y.year
+),
+MaleDiplomatsDiff AS (
+    SELECT 
+        year, 
+        maleDiplomats,
+        LAG(maleDiplomats) OVER (ORDER BY Year) AS male_prevYearCount,
+        maleDiplomats - LAG(maleDiplomats) OVER (ORDER BY Year) AS male_diff
+    FROM 
+        DiplomatsCounts
+),
+malePct_MAX AS (
+	SELECT 
+		CONCAT(MAX(pct), '%') AS malePct_MAX
+	FROM (
+		SELECT 
+			year, 
+			maleDiplomats, 
+			male_prevYearCount, 
+			male_diff,
+			CAST(ROUND(((CONVERT(DECIMAL(18,2), male_diff) / male_prevYearCount) * 100), 2) AS DECIMAL(18,2)) AS pct
+		FROM 
+			MaleDiplomatsDiff) AS sub )
+SELECT malePct_MAX
+FROM malePct_MAX AS MM
+
+-- 2) risultato anche con anno ma più complicato.
+WITH DiplomatsCounts AS (
+    SELECT 
+        Y.year, 
+        COUNT(D.gender) AS maleDiplomats
+    FROM 
+        dbo.targetArea AS TA
+        LEFT JOIN dbo.sendingCountries AS SC ON TA.sendingCountryID = SC.sendingCountryID
+        LEFT JOIN dbo.vw_country_GROUP_BY AS VW ON SC.countryID = VW.countryID
+        LEFT JOIN dbo.diplomats AS D ON TA.diplomatID = D.diplomatID
+        LEFT JOIN dbo.years AS Y ON TA.yearID = Y.yearID
+    WHERE 
+        VW.alpcode = 'ITA' AND D.gender = 0
+    GROUP BY 
+        Y.year
+),
+MaleDiplomatsDiff AS (
+    SELECT 
+        year, 
+        maleDiplomats,
+        LAG(maleDiplomats) OVER (ORDER BY Year) AS male_prevYearCount,
+        maleDiplomats - LAG(maleDiplomats) OVER (ORDER BY Year) AS male_diff
+    FROM 
+        DiplomatsCounts
+),
+MaxPct AS (
+    SELECT 
+        MAX(pct) AS max_pct
+    FROM (
+        SELECT 
+            year, 
+            CAST(ROUND(((CONVERT(DECIMAL(18,2), male_diff) / male_prevYearCount) * 100), 2) AS DECIMAL(18,2)) AS pct
+        FROM 
+            MaleDiplomatsDiff
+    ) AS Subquery
+)
+SELECT 
+    CONCAT(max_pct, '%') AS malePct_MAX,
+    year AS year_malePct_MAX
+FROM 
+    MaxPct
+JOIN 
+    (
+        SELECT 
+            year, 
+            CAST(ROUND(((CONVERT(DECIMAL(18,2), male_diff) / male_prevYearCount) * 100), 2) AS DECIMAL(18,2)) AS pct
+        FROM 
+            MaleDiplomatsDiff
+    ) AS Subquery ON MaxPct.max_pct = Subquery.pct;
 
 ---
 
@@ -107,6 +187,26 @@ FROM (
 			LEFT JOIN dbo.years AS Y ON TA.yearID = Y.yearID
 		GROUP BY VW.country_GROUP_BY ) AS sub
 WHERE country_GROUP_BY = 'Italy'; -- 21,60% 57esima su 203
+
+-- Crescita media % annuale.
+WITH CTE AS (
+	SELECT year, media, LAG(media) OVER (ORDER BY year) AS avgPct_prevYear,
+			media - LAG(media) OVER (ORDER BY year) AS annual_avgPct_growth
+	FROM (
+			SELECT 
+				Y.year, 
+				CAST(ROUND(AVG(COALESCE(FL.femaleLegislatorPercentage, 0)), 2) AS DECIMAL(18,2)) AS media
+			FROM
+				dbo.targetArea AS TA
+				LEFT JOIN dbo.sendingCountries AS SC ON TA.sendingCountryID = SC.sendingCountryID
+				LEFT JOIN vw_country_GROUP_BY AS VW ON SC.countryID = VW.countryID
+				LEFT JOIN dbo.femaleLegislators AS FL ON SC.femaleLegislatorID = FL.femaleLegislatorID
+				LEFT JOIN dbo.years AS Y ON TA.yearID = Y.yearID
+			WHERE VW.alpcode = 'ITA'
+			GROUP BY Y.year) AS sub )
+SELECT CAST(ROUND((SUM(annual_avgPct_growth)/(COUNT(year)-1)),2) AS DECIMAL(18,2)) AS avgPct_growth
+FROM CTE;
+-- avgPct_growth: 3,67
 
 
 
